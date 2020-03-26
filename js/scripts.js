4d3b08f8296404c77602d078d356f6f0b67709ca
Yassiribararh <yassiribararh@gmail.com>
2020-03-26 21:49:09 +0000

    edit slider and add previous and next buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,34 @@ console.log("hello");
 
 
 // script for the slideshow
-var myIndex = 0;
+let myIndex = 0;
 carousel();
 
+
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
+  let i;
+  const x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}
+
   x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 6000); // Change image every 2 seconds
-}
+  setTimeout(carousel, 3000); // Change image automatically
+
+
+};
+
+
+// JavaScript handles to our previous and next buttons
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('previous');
+
+// EventListeners for our previous and next buttons
+nextBtn.addEventListener('click', ev => {
+   const nextslide = carousel() + 1;
+});
+prevBtn.addEventListener('click', ev => {
+   const npreviouslide = carousel() - 1;
+});
