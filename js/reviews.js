38ee12d258951808e76d4b32b18a7229d82c8f64
Yassiribararh <yassiribararh@gmail.com>
2020-03-31 22:15:22 +0100

    fixed the js code for the reviews section/fixed the footer position/ fixed the styles/ added a logo to the header

--- js/reviews.js
+++ js/reviews.js
@@ -1,48 +1,56 @@
 "use strict";
 
+
+function submit() {
+  alert("Thanks! your message has been submited");
+}
+
+//script for clicking the submit button
+const submited = document.getElementById('submit');
+
+submited.addEventListener("click", submit);
 //script for reviews section (() => {
 (() => {
   // all existing code goes here
   const listElement = document.getElementById('reviews');
 
+  //function to add html item
   function addItem(item) {
     const itemElement = document.createElement('li');
     itemElement.textContent = item;
-                                              // <- new
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'x';
+    itemElement.appendChild(deleteButton);
+    // Event listener
+    deleteButton.addEventListener('click', ev => { // <- new
+      listElement.removeChild(itemElement);        // <- new
+    });
     listElement.appendChild(itemElement);
-
   };
 
-
-   // list array using for Each
-  const list = ['User01: Great site', 'User02: YNWA!', 'User03: Easy to navigate!'];
+  // list array using for Each
+  const list = ['User01: Great site, easy access to pages', 'User02: YNWA! We love liverpool in Morocco', 'User03: Easy to navigate! love how the news are up to date!'];
   list.forEach(item => { addItem(item); } );
 
 
+  function clearList() {
+    while(listElement.firstChild) {
+      listElement.removeChild(listElement.firstChild);
+    }
+  }
+
+  // alert the user that his review was submited
+  function greeting() {
+    alert("Thanks! your review has been submited");
+  }
+
+  function renderList(list) {
+    list.forEach(addItem);
+  }
   // JavaScript handles to our new elements
   const newItem = document.getElementById('newItem');
   const addBtn = document.getElementById('addBtn');
   const clearBtn = document.getElementById('clearBtn');
-
-  //saving the list
-  function save() {
-    const items = [...listElement.childNodes];
-    if(items.length) {
-      const list = items.map(item => {
-        return item.textContent.slice(0, -1);
-      });
-      localStorage.setItem('reviews-list', list);
-    } else {
-      localStorage.removeItem('reviews-list');
-    }
-  }
- //lOAD REVIEWS LIST
- function load() {
-    const reviewsList = localStorage.getItem('reviews-list');
-    if(reviewsList) {
-      addList(reviewsList.split(','));
-    }
-  }
 
   // Event listeners
   addBtn.addEventListener('click', ev => {
@@ -52,28 +60,41 @@
       }
     });
     newItem.value = null;
-    save();
   });
 
-  addBtn.addEventListener('click', ev => {
-    addItem(newItem.value.split(','));
-    newItem.value = null;
-    save();
+  clearBtn.addEventListener('click', ev => {
+    clearList();
   });
 
+  addBtn.addEventListener("click", greeting);
 
+  //Saving the list
+  window.addEventListener('beforeunload', ev => {
+    const items = [...listElement.childNodes];
+    if(items.length) {
+      const list = items.map(item => {
+        return item.textContent.slice(0, -1);
+      });
+      localStorage.setItem('review-list', list);
+    } else {
+      localStorage.removeItem('review-list');
+    }
+  });
 
-  // Saving data for later use
-    window.addEventListener('beforeunload', save);
+  //Event Listener
+  window.addEventListener('DOMContentLoaded', ev => {
+    const reviewList = localStorage.getItem('review-list');
+    if(reviewList) {
+      clearList();
+      renderList(reviewList.split(','));
+    }
+  });
 
-    // Loading data from local storage
-    window.addEventListener('DOMContentLoaded', load);
+  //Upgrade the interface
+  newItem.addEventListener("keyup", ev => {
+    if (ev.key === "Enter") {
+      addBtn.click();
+    }
+  });
 
-    window.addEventListener('storage', ev => {
-      if(ev.key == "reviews-list") {
-        clearList();
-        load();
-      }
-    })
-
-})();
+})()
